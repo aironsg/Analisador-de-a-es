d93d701a3cbafafe4a37c1d5a893701d833d2c85
Airon gomes <84585237+aironsg@users.noreply.github.com>
2025-01-12 22:11:38 -0300

    Adicionando opção de datas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-
+from datetime import timedelta as td
 NAME_STOCK = 'ITUB4.SA'
 locks = ['ITUB4.SA','PETR4.SA','MGLU3.SA','VALE3.SA','ABEV3.SA','GGBR4.SA']
 
@@ -15,9 +15,10 @@ def loading_data(enterprises):
 
 data = loading_data(locks)
 
+st.sidebar.header('Filtros')
 
 # filtros de visualizações
-locks_list = st.multiselect('Escolha as ações para visualizar',data.columns)
+locks_list = st.sidebar.multiselect('Escolha as ações para visualizar',data.columns)
 if locks_list:
     data = data[locks_list]
     if len(locks_list) == 1:
@@ -29,5 +30,14 @@ st.write('''
 O gráfico abaixo representa a evolução do preço das ações da IBOVESPA.
 
 ''')
+
+date_initial = data.index.min().to_pydatetime()
+date_final = data.index.max().to_pydatetime()
+days = 1
+interval_date =  st.sidebar.slider('Escolha um período',
+                                   min_value=date_initial, max_value=date_final,
+                                   value=(date_initial, date_final),
+                                   step=td(days=days))
+data = data.loc[interval_date[0]:interval_date[1]]
 
 st.line_chart(data=data)
